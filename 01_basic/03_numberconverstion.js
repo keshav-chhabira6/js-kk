194469bdf0a16331af306c7e3ee392fdf79fc945
keshav kumar <keshavchhabira@gmail.com>
2025-07-23 11:36:19 +0000

    change in location of files

---
+++ 01_basic/03_numberconverstion.js
@@ -0,0 +1,41 @@
+//let score = "10"
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInnumber = Number(score)
+
+//console.log( valueInnumber);
+
+// let isLoggedIn = ""
+// let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn);
+
+
+
+// let number = 10 
+
+//  let stringNumber = String(number)
+
+// console.log(typeof stringNumber);
+
+
+// ******************* operatiions *****************
+
+
+let value = 3 
+let negvalue = -value 
+console.log(negvalue);
+
+
+
+let str1 = "keshav "
+let str2 = " kuamr"
+
+
+let str3 = str1+str2
+
+console.log(str3);
+
