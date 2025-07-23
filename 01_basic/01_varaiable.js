256671fb6628bfb2928a48a2f9bc38a80a6f9840
keshav kumar <keshavchhabira@gmail.com>
2025-07-23 09:45:47 +0000

    this is my container config done

---
+++ 01_basic/01_varaiable.js
@@ -0,0 +1,19 @@
+const accountId = 418491
+let accountEmail = "keshav@gmail.com"
+var accountPassword = "56787"
+accountCity = "Qetta "
+let accountstate;
+
+// accountId = 2 // not allowed  
+
+accountEmail = " hcc"
+accountPassword = " 1234"
+accountCity = "islamabad"
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountstate])
+
+
+
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";// treat all js code with new version 
+
+ 
+
+console.log("keshav");
+
+let name = " keshav "
+let age = " 22 "
+let isLoggedIn = false
+
+
+console.log(typeof false);
+
+
+
